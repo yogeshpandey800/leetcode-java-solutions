9a0cd6578f7ed9161048d1630250cabbbf4d1069
Yogesh <ypandey041@gmail.com>
2026-05-29 18:53:09 +0530

    Add Arrays folder with TwoSum solution

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,25 @@
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        
+        for(int i = 0; i<nums.length; i++){
+
+            for(int j = i+1; j<nums.length; j++){
+
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args){
+        TwoSum sol = new TwoSum();
+        int[] nums = {2,7,11,15};
+        int target = 9;
+
+        int[] result = sol.twoSum(nums, target);
+
+        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+    }
+}
